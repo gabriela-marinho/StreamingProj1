5c938368d3e82be07f6c2e034e58d5004cd5cad9
gabriela-marinho <g.barreto.marinho@gmail.com>
2021-12-13 00:49:47 -0300

    colocado hash na senha

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { CreateUsersDto } from './dto/create-users.dto';
 import { PrismaService } from 'src/prisma.service';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
   constructor(private db: PrismaService) {}
 
   async createUser(data: CreateUsersDto) {
-    delete data.idade;
-    await this.db.user.create({ data });
+    const userExists = await this.db.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (userExists) {
+      throw new ConflictException('email ja cadastrado,use outro!');
+    }
+    const saltos = 10;
+    const hashSenha = await bcrypt.hash(data.senha, saltos);
   }
 }
