0274b79e21e16d8a54f4e7c15c81625a94c98629
gabriela-marinho <g.barreto.marinho@gmail.com>
2021-12-20 03:38:04 -0300

    terminado o crud de filmes

--- src/filmes/filmes.controller.ts
+++ src/filmes/filmes.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FilmesService } from './filmes.service';
 import { CreateFilmeDto } from './dto/create-filme.dto';
 import { UpdateFilmeDto } from './dto/update-filme.dto';
-
+import { Filme } from '@prisma/client';
 @Controller('filmes')
 export class FilmesController {
   constructor(private readonly filmesService: FilmesService) {}
 
-  @Post()
-  create(@Body() createFilmeDto: CreateFilmeDto) {
+  @Post('create')
+  create(@Body() createFilmeDto: CreateFilmeDto): Promise<Filme> {
     return this.filmesService.create(createFilmeDto);
   }
 
-  @Get()
-  findAll() {
+  @Get('all')
+  findAll(): Promise<Filme[]> {
     return this.filmesService.findAll();
   }
+  // não tem um tipo de retorno mas posso colocar, pois é uma boa partica
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.filmesService.findOne(+id);
+  @Get('find-one/:id')
+  findOne(@Param('id') id: string): Promise<Filme> {
+    return this.filmesService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFilmeDto: UpdateFilmeDto) {
-    return this.filmesService.update(+id, updateFilmeDto);
+  @Patch('update/:id')
+  update(
+    @Param('id') id: string,
+    @Body() updateFilmeDto: UpdateFilmeDto,
+  ): Promise<Filme> {
+    return this.filmesService.update(id, updateFilmeDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.filmesService.remove(+id);
+  @Delete('delete/:id')
+  remove(@Param('id') id: string): Promise<{ message: string }> {
+    return this.filmesService.remove(id);
   }
 }
--- src/filmes/filmes.service.ts
+++ src/filmes/filmes.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateFilmeDto } from './dto/create-filme.dto';
 import { UpdateFilmeDto } from './dto/update-filme.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -9,26 +13,66 @@ export class FilmesService {
   // preciso desse construtor para o meu 'this' poder acessar meu banco
   constructor(private db: PrismaService) {}
 
-  async create(createFilmeDto: CreateFilmeDto) {
+  // a linha de codigo abaixo procura no banco e preenche
+  // a variavel :filmExist, preenchendo a mesma
+  // entra no if, caso seja encontrada,preciso parar o create
+  // usando o throw new
+  async create(createFilmeDto: CreateFilmeDto): Promise<Filme> {
     // findunique sempre pede um objeto
     const filmExist = await this.db.filme.findUnique({
-
+      where: { nomeFilme: createFilmeDto.nomeFilme },
     });
+    if (filmExist) {
+      throw new ConflictException('Nome do Filme já cadastrado!');
+    }
+    const filme = await this.db.filme.create({ data: createFilmeDto });
+    return filme;
+  }
+  // so tem parametro de saída nao tem de entrada
+  // primeiro faz uma consulta no banco, depois retorna a consulta
+  // passando o mouse em cima de filme (return) vemos q a promessa
+  // é um VETOR,logo a promisse tem q ter um vetor
+  async findAll(): Promise<Filme[]> {
+    const filme = await this.db.filme.findMany();
+    return filme;
+  }
+  // a promose aqui só quer encontrar uma retorno e nao todos
+  async findOne(id: string): Promise<Filme> {
+    const filmExist = await this.db.filme.findUnique({
+      // qnd vc tem o mesmo nome da propriedade com o mesmo nome do parametyro
+      // q está passando pode deixar só um nome, no nosso caso é o id
+      where: { id },
+    });
+    if (!filmExist) {
+      throw new NotFoundException('Filme com esse id não encontrado!');
+    }
+    return filmExist;
   }
 
-  findAll() {
-    return `This action returns all filmes`;
+  async update(id: string, updateFilmeDto: UpdateFilmeDto): Promise<Filme> {
+    const filmExist = await this.db.filme.update({
+      // o update recebe 2 itens obrigatorios :where e o data.
+      data: updateFilmeDto,
+      where: { id },
+    });
+    return filmExist;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} filme`;
-  }
-
-  update(id: number, updateFilmeDto: UpdateFilmeDto) {
-    return `This action updates a #${id} filme`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} filme`;
+  async remove(id: string): Promise<{ message: string }> {
+    const filmExist = await this.db.filme.findUnique({
+      // qnd vc tem o mesmo nome da propriedade com o mesmo nome do parametyro
+      // q está passando pode deixar só um nome, no nosso caso é o id
+      where: { id },
+    });
+    if (!filmExist) {
+      throw new NotFoundException('Filme com esse id não encontrado!');
+    } else {
+      // delete precisa de um where obrigatorio
+      await this.db.filme.delete({
+        where: { id },
+      });
+    }
+    // return { message: 'Filme com id encontrado e deletado' };
+    return { message: `'Filme com ${id} encontrado e deletado'` };
   }
 }
